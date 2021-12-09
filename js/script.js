55ae76e77dd4271ffa103cf585bbe66fc1f89a0f
ElperdonXV <davidemartinografico@gmail.com>
2021-12-09 16:00:04 +0100

    all done with bonus

--- js/script.js
+++ js/script.js
@@ -55,3 +55,39 @@ function printCards(array, container) {
 
 const container = document.querySelector('.team-container');
 printCards(team, container);
+
+
+//BONUS 
+//Utilizziamo gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team:
+//cliccando sul pulsante “add” viene creato un nuovo oggetto, il quale viene inserito nell’array iniziale, 
+//e viene stampata una nuova card con tutte le informazioni inserite dall’utente.
+
+const inputName = document.getElementById('name');
+const inputRole = document.getElementById('role');
+const inputImage = document.getElementById('image');
+const button = document.getElementById('addMemberButton');
+
+button.addEventListener('click', function (event) {
+  event.preventDefault(); //previene il refresh della pagina in un form
+
+  //prendiamo dati inseriti dall'utente
+  const nome = inputName.value;
+  const ruolo = inputRole.value;
+  const foto = inputImage.value;
+
+  if (nome.length > 0 && ruolo.length > 0  && foto.length > 0) {
+    //se ha inserito effettivamente qualcosa
+    const obj = {
+      name: nome,
+      role: ruolo,
+      image: foto
+    };
+    team.push(obj);
+
+    //ristampiamo tutte le cards
+    const container = document.querySelector('.team-container');
+    printCards(team, container);
+  } else {
+    console.log('Non hai inserito i dati correttamente!');
+  }
+});
